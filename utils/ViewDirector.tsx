2eac9e9b6f2ef31ee4861a012e01e3d7245451a2
Michael Nunan <michael.nunan@outlook.com>
2024-07-24 20:44:18 -0500

    noauth pages

--- utils/ViewDirector.tsx
+++ utils/ViewDirector.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useAuth } from '@/utils/context/AuthContext';
 import Loading from '@/components/Loading';
 import NavHeader from '@/components/NavHeader';
